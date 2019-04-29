bf4b27a2eef726c3a6cdba116bf52587498f2dc9
brenomol1987 <emaildobrenomol@gmail.com>
2019-04-29 09:09:21 -0300

    Mudanças tela de proposta 0000019

--- views/pregao-eletronico/propostas/filtro.php
+++ views/pregao-eletronico/propostas/filtro.php
@@ -1,9 +1,9 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
-  <h1>Pesquisa de Proposta</h1>
+  <h1>Cadastro de Proposta</h1>
   <ol class="breadcrumb">
     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-    <li class="active">Pesquisa de Proposta</li>
+    <li class="active">Cadastro de Proposta</li>
   </ol>
 </section>
 
@@ -13,7 +13,7 @@
   <!-- Default box -->
   <div class="box">
     <div class="box-header with-border">
-      <h3 class="box-title">Selecione os dados para busca</h3>
+      <!--<h3 class="box-title">Selecione os dados para busca</h3>-->
     </div>
     <div class="box-body">
         
@@ -22,10 +22,36 @@
         <form action="" method="post" enctype="multipart/form-data">
             <div class="row">
                 <div class="form-group col-md-4">
-                  <label for="uf">Uf</label>
+                  <label for="uf">UF</label>
                   <select name="uf" required="" class="form-control">
                     <option value="">Selecione</option>
-                    <option value="MG">MG</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
                   </select>
                 </div>
             </div>
