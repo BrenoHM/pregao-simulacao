920db222e8e29127c7ca4b9f8015fae8cec7a500
brenomol1987 <emaildobrenomol@gmail.com>
2019-04-29 08:58:08 -0300

    Mudanças tela inicial Item 0000016

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -1,10 +1,10 @@
 <section class="content-header">
-    <h1>Página Inicial</h1>
+    <h1>Simulador de Pregão Online</h1>
     <ol class="breadcrumb">
         <li><a href="<?php echo BASE_URL ?>/"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Página Inicial</li>
+        <li class="active">Simulador de Pregão Online</li>
     </ol>
 </section>
 <section class="content">
-	
+	<h3>Aqui você poderá simular quantas vezes quiser as principais funcionalidades de um pregão online, baseado na plataforma mais utilizada para compras governamentais: o Comprasnet.</h3>
 </section>
